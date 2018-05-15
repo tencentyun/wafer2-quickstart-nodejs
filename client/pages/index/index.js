9e35fb4e0befd55569d479c87573f9d2ddd7f622
judeyuan(袁梦珠) <judeyuan@tencent.com>
2018-05-15 11:33:06 +0800

    增加登录态检测

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -57,43 +57,75 @@ Page({
 
     bindGetUserInfo: function (e) {
       if (this.data.logged) return;
+
       util.showBusy('正在登录');
+
       var that = this;
-   
+      var userInfo = e.detail.userInfo;
+
       // 查看是否授权
       wx.getSetting({
         success: function (res) {
           if (res.authSetting['scope.userInfo']) {
 
-            wx.login({
-              success: function (loginResult) {
-                var loginParams = {
-                  code: loginResult.code,
+            // 检查登录是否过期
+            wx.checkSession({
+              success: function () {
+                // 登录态未过期
+                util.showSuccess('登录成功');
+                that.setData({
+                  userInfo: userInfo,
+                  logged: true
+                })
+              },
+
+              fail: function () {
+                qcloud.clearSession();
+                // 登录态已过期，需重新登录
+                var options = {
                   encryptedData: e.detail.encryptedData,
                   iv: e.detail.iv,
+                  userInfo: userInfo
                 }
-                qcloud.requestLogin({loginParams, success(){
-                    util.showSuccess('登录成功');
-                    that.setData({
-                      userInfo: e.detail.userInfo,
-                      logged: true
-                    })
-                  },
-                  fail(error) {
-                    util.showModel('登录失败', error)
-                    console.log('登录失败', error)
-                  }
-                });
-              },
-              fail: function (loginError) {
-                util.showModel('登录失败', loginError)
-                console.log('登录失败', loginError)
+                that.doLogin(options);
               },
             });
           } else {
             util.showModel('用户未授权', e.detail.errMsg);
           }
         }
+      });
+    },
+
+    doLogin: function(options) {
+      var that = this;
+
+      wx.login({
+        success: function (loginResult) {
+          var loginParams = {
+            code: loginResult.code,
+            encryptedData: options.encryptedData,
+            iv: options.iv,
+          }
+          qcloud.requestLogin({
+            loginParams, success() {
+              util.showSuccess('登录成功');
+
+              that.setData({
+                userInfo: options.userInfo,
+                logged: true
+              })
+            },
+            fail(error) {
+              util.showModel('登录失败', error)
+              console.log('登录失败', error)
+            }
+          });
+        },
+        fail: function (loginError) {
+          util.showModel('登录失败', loginError)
+          console.log('登录失败', loginError)
+        },
       });
     },
 
