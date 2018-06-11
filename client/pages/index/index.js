0367c71f156429e20b1137141b89fac59fcf024a
Jason <ceocjy@vip.qq.com>
2018-06-11 23:22:22 +0800

    Update: refactor login and request lib

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -55,78 +55,21 @@ Page({
         })
     },
 
-    bindGetUserInfo: function (e) {
-      if (this.data.logged) return;
+    bindGetUserInfo: function () {
+        if (this.data.logged) return
 
-      util.showBusy('正在登录');
+        util.showBusy('正在登录')
 
-      var that = this;
-      var userInfo = e.detail.userInfo;
-
-      // 查看是否授权
-      wx.getSetting({
-        success: function (res) {
-          if (res.authSetting['scope.userInfo']) {
-
-            // 检查登录是否过期
-            wx.checkSession({
-              success: function () {
-                // 登录态未过期
-                util.showSuccess('登录成功');
-                that.setData({
-                  userInfo: userInfo,
-                  logged: true
-                })
-              },
-
-              fail: function () {
-                qcloud.clearSession();
-                // 登录态已过期，需重新登录
-                var options = {
-                  encryptedData: e.detail.encryptedData,
-                  iv: e.detail.iv,
-                  userInfo: userInfo
-                }
-                that.doLogin(options);
-              },
-            });
-          } else {
-            util.showModel('用户未授权', e.detail.errMsg);
-          }
-        }
-      });
-    },
-
-    doLogin: function(options) {
-      var that = this;
-
-      wx.login({
-        success: function (loginResult) {
-          var loginParams = {
-            code: loginResult.code,
-            encryptedData: options.encryptedData,
-            iv: options.iv,
-          }
-          qcloud.requestLogin({
-            loginParams, success() {
-              util.showSuccess('登录成功');
-
-              that.setData({
-                userInfo: options.userInfo,
-                logged: true
-              })
+        qcloud.loginWithCode({
+            success: res => {
+                this.setData({ userInfo: res, logged: true })
+                util.showSuccess('登录成功')
             },
-            fail(error) {
-              util.showModel('登录失败', error)
-              console.log('登录失败', error)
+            fail: err => {
+                console.error(err)
+                util.showModel('登录错误', err.message)
             }
-          });
-        },
-        fail: function (loginError) {
-          util.showModel('登录失败', loginError)
-          console.log('登录失败', loginError)
-        },
-      });
+        })
     },
 
     // 切换是否带有登录态
