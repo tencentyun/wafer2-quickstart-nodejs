b9431b95c93795324753c812b181a5d4ec45d983
Jason <ceocjy@vip.qq.com>
2018-06-12 00:25:27 +0800

    Remove unused code

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -12,49 +12,6 @@ Page({
     },
 
     // 用户登录示例
-    login: function() {
-        if (this.data.logged) return
-
-        util.showBusy('正在登录')
-        var that = this
-
-        // 调用登录接口
-        qcloud.login({
-            success(result) {
-                if (result) {
-                    util.showSuccess('登录成功');
-                    that.setData({
-                        userInfo: result,
-                        logged: true
-                    })
-                } else {
-                    // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
-                    qcloud.request({
-                        url: config.service.requestUrl,
-                        login: true,
-                        success(result) {
-                            util.showSuccess('登录成功')
-                            that.setData({
-                                userInfo: result.data.data,
-                                logged: true
-                            })
-                        },
-
-                        fail(error) {
-                            util.showModel('请求失败', error)
-                            console.log('request fail', error)
-                        }
-                    })
-                }
-            },
-
-            fail(error) {
-                util.showModel('登录失败', error)
-                console.log('登录失败', error)
-            }
-        })
-    },
-
     bindGetUserInfo: function () {
         if (this.data.logged) return
 
